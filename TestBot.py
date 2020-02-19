d41e6e0bbeceda6fdd4b2d4d0b2f460099ac51b5
전세훈 <twilivez@gmail.com>
2020-02-19 23:41:10 +0900

    테스트 : Heroku 테스트

--- TestBot.py
+++ TestBot.py
@@ -14,21 +14,12 @@ async def on_ready():
     print(bot.user.id)
     print("Bot status: online")
     print("Bot command prefix : " + bot.command_prefix) 
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Testing"))
-
-@bot.command()
-async def add(ctx, todo:str):
-    todoList.append(todo)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Heroku Test"))
 
 @bot.command()
 async def show(ctx):
-    for todo in todoList:
-        await ctx.send(todo)
-
-@bot.command()
-async def showembed(ctx):
-    embed = discord.Embed(title="제목", description="설명", color=0x00ff00)
-    embed.set_footer(text="푸터")
+    embed = discord.Embed(title="Test", description="Descriptions", color=0x00ff00)
+    embed.set_footer(text="footer")
     await ctx.send(embed=embed)
 
 token = os.environ["BOT_TOKEN"]
