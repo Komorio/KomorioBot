0aac800c7ce2ff903239cd4209e025cdf87c0c63
전세훈 <twilivez@gmail.com>
2020-03-20 01:29:33 +0900

    [ADD] 기본적인 Database 구축

---
+++ Database.py
@@ -0,0 +1,57 @@
+import sqlite3 as sql
+
+
+dbName = "komoriobot.db"
+
+def AddTable(tableName:str):
+    db = sql.connect(dbName)
+    cursor = db.cursor()
+
+    command = "CREATE TABLE {0} (title text, description text, footer text)".format(tableName)
+    cursor.execute(command)
+    
+    db.close()
+
+def DropTable(tableName:str):
+    db = sql.connect(dbName)
+    cursor = db.cursor()
+
+    command = "DROP TABLE {0}".format(tableName)
+    cursor.execute(command)
+
+    db.close()
+
+
+def InsertData(tableName:str, title:str = "", description:str ="", footer:str =""):
+    db = sql.connect(dbName)
+    cursor = db.cursor()
+        
+    command = "INSERT INTO {0} VALUES('{1}', '{2}', '{3}')".format(tableName, title, description, footer)
+    cursor.execute(command)
+    db.commit()
+
+    db.close()
+
+
+def DeleteData(tableName:str, title:str =""):
+    db = sql.connect(dbName)
+    cursor = db.cursor()
+
+    command = "DELETE FROM {0} WHERE title = '{1}'".format(tableName, title)
+    cursor.execute(command)
+    db.commit()
+    
+    db.close()
+
+
+def GetAllData(tableName:str):
+    db = sql.connect(dbName)
+    cursor = db.cursor()
+
+    command = "SELECT * FROM {0}".format(tableName)
+    cursor.execute(command)
+    result = cursor.fetchall()
+    return result
+
+    db.close()
+
