51e15500e28a58b07dc2c33eaa35b3607e640acc
전세훈 <twilivez@gmail.com>
2020-03-20 07:11:08 +0900

    [ADD] 봇이 출력할 정보를 갖고 있을 스크립트 추가

---
+++ BotInformation.py
@@ -0,0 +1,15 @@
+def GetBotExplanation():
+    explanation = "기본적으로 Komorio가 Python Discrod Bot 학습을 위해 만들어졌습니다. \n \n"
+    explanation += "다양한 기능을 통해 Discord에서 일상을 도와줍니다. \n \n"
+    explanation += "근데 더이상 쓸 설명이 없네 ㄴㅇㄱ \n \n"
+    return explanation
+
+def GetCommands():
+    commands = [""]
+    commands[0] = "🛠 테스트 : 현재 개발중인 커맨드를 테스트합니다. \n"
+    commands.append("📒 설명 : 이 봇에 대해 설명합니다. \n")
+    commands.append("🔩 명령어 : 이 봇이 갖고 있는 명령어를 설명합니다. \n") 
+
+    return commands
+     
+
